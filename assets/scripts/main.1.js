7ce271ca749edf07f1d91b069c2bb918591e91b0
Kyle Harrison <redactedprofile@gmail.com>
2017-12-06 14:17:44 -0800

    latest update to alexs site

--- assets/scripts/main.1.js
+++ assets/scripts/main.1.js
@@ -2,41 +2,51 @@
 var portfolio = {
 	init: function () {
 	var self = this;
+	var self_setTitle = self.setTitle;
 	$('#statement').hide(0, function() {
 		$("div.leftSection").slideDown('slow', function () {
 			console.log(location.hash.slice(1,99));
 			switch (location.hash.slice(1,99)) {
 				case "skills":
 				$('div#skills').slideDown('fast');
-				 self.setTitle("Education and Skills");
+				 self_setTitle("Education and Skills");
 				 break;
 				case "companies":
 				$('#companies').slideDown('fast');
-				 self.setTitle("Companies I've worked for");
+				 self_setTitle("Companies I've worked for");
 				 break;
 				case "hateMachine":
 				$('#hateMachine').slideDown('fast');
-				 self.setTitle("Internet Hate Machine - UI edition");
+				 self_setTitle("Internet Hate Machine");
 				break;
 				case "resume":
 				$('div#resume').slideDown('fast');
-				self.setTitle("Resume");
+				self_setTitle("Resume");
 				break;
 				case "frontend":
 				$('div#frontend').slideDown('fast');
-				 self.setTitle("Front end Samples");
+				 self_setTitle("Front end Samples");
 				break;
 				case "backend":
 				$('div#backend').slideDown('fast');
-				self.setTitle("Back end Samples");
+				self_setTitle("Back end Samples");
 				break;
 				case "flash":
 				$('div#flash').slideDown('fast');
-			 	self.setTitle("Flash Samples");
+			 	self_setTitle("Flash Samples");
+			 	break;
+				case "humor":
+				$('div#humor').slideDown('fast');
+			 	self_setTitle("Funny ramblings for future writing");
+			 	break;
+				case "politics":
+				$('div#politics').slideDown('fast');
+			 	self_setTitle("Politics");
 			 	break;
 			 	default:
 				$("#statement").fadeIn('slow');
-				self.setTitle("Personal Statement");
+				self_setTitle("Personal Statement");
+				break
 			}
 		});
 	});
@@ -45,50 +55,60 @@ var portfolio = {
 	},
 	setEvents: function () {
 		var self = this;
+		var self_setTitle = self.setTitle;
 		$('.educationHref').on("click", function() {
-			console.log("hi")
 			self.clearFades();
 			 $('div#skills').slideDown('fast');
-			 self.setTitle("Education and Skills");
+			 self_setTitle("Education and Skills");
 		});
-		
 		$('.companyHref').on("click", function() {
 			self.clearFades();
 			 $('#companies').slideDown('fast');
-			 self.setTitle("Companies I've worked for");
+			 self_setTitle("Companies I've worked for");
 		});
+		$('.humorHref').on("click", function() {
+			self.clearFades();
+			 $('#humor').slideDown('fast');
+			 self_setTitle("Funny ramblings for future writing");
+		});
+
 		$('.boredHref').on("click", function() {
 			self.clearFades();
 			 $('#hateMachine').slideDown('fast');
-			 self.setTitle("Internet Hate Machine - UI edition");
+			 self_setTitle("Internet Hate Machine - UI edition");
+		});
+		$('.politicsHref').on("click", function() {
+			self.clearFades();
+			 $('#politics').slideDown('fast');
+			 self_setTitle("Politics");
 		});
 		$('.resumeHref').on("click", function() {
 			self.clearFades();
 			 $('div#resume').slideDown('fast');
-			 self.setTitle("Resume");
+			 self_setTitle("Resume");
 		});
 		$('.personalHref').on("click", function() {
 			self.clearFades();
 			 $('div#statement').slideDown('fast');
-			 self.setTitle("Personal Statement");
+			 self_setTitle("Personal Statement");
 		});
 		
 		$('.frontHref').on("click", function() {
 			self.clearFades();
 			 $('div#frontend').slideDown('fast');
-			 self.setTitle("Front end Samples");
+			 self_setTitle("Front end Samples");
 		});
 		
 		$('.backHref').on("click", function() {
 			self.clearFades();
 			 $('div#backend').slideDown('fast');
-			 self.setTitle("Back end Samples");
+			 self_setTitle("Back end Samples");
 		});
 		
 		$('.flashHref').on("click", function() {
 			self.clearFades();
 			 $('div#flash').slideDown('fast');
-			 self.setTitle("Flash Samples");
+			 self_setTitle("Flash Samples");
 		});
 	},
 	didYouKnow: function () {
@@ -106,7 +126,16 @@ var portfolio = {
 		}).fadeIn('medium');
 	},
 	clearFades: function () {
-		$('#statement,#hateMachine,#skills,#companies,#resume,#frontend,#backend,#flash').slideUp('slow').delay(600);
+		$("#statement,"
+		 +"#hateMachine,"
+		 +"#politics,"
+		 +"#skills,"
+		 +"#humor,"
+		 +"#companies,"
+		 +"#resume,"
+		 +"#frontend,"
+		 +"#backend,"
+		 +"#flash").slideUp('slow').delay(600);
 		$("a").blur();
 	}
 }
